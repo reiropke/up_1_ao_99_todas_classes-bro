8a63c4ec384d208129426599b822f65c6cc3923b
reiropke <35391209+reiropke@users.noreply.github.com>
2018-01-19 03:06:07 -0200

    Update quest-c-2.pm
    Poderei fazer mais mudanças e testar elas assim que chegar em casa...

--- classes/cavaleiro-runico/quest-c-2.pm
+++ classes/cavaleiro-runico/quest-c-2.pm
@@ -15,3 +15,88 @@ sub initParamsQuestClasse2 {
     Commands::run ("iconf 946 5 1 0" ); #Casco de Caramujo
 }
 
+#Copiadode Gaspel, CALF e Nipodemos e editado por reiropke
+#quest virar cavaleiro
+
+automacro virarCavaleiroInicio_salvarEmProntera {
+    JobID $paramsClasses{idC1}
+    JobLevel = 50
+    FreeSkillPoints = 0
+    priority 2
+    exclusive 1
+    ConfigKeyNot saveMap prontera
+    ConfigKeyNot In_saveMap_sequence true
+    ConfigKeyNot quest_eden em_curso
+    ConfigKeyNot quest_eden terminando
+    NpcNotNear /Chefe dos Cavaleiros/
+    #testar quests
+    QuestInactive 9000
+    QuestInactive 9001
+    QuestInactive 9002
+    QuestInactive 9003
+    QuestInactive 9004
+    QuestInactive 9005
+    QuestInactive 9006
+    QuestInactive 9007
+    QuestInactive 9008
+    QuestInactive 9009
+    QuestInactive 9010
+    QuestInactive 9011
+    QuestInactive 9012
+    call SetSaveIn "prontera"
+}
+
+automacro virarCavaleiro_Começo_irAoNpc {
+    JobLevel = 50
+    JobID $paramsClasses{idC1}
+    exclusive 1
+    QuestInactive 9000
+    QuestInactive 9001
+    QuestInactive 9002
+    QuestInactive 9003
+    QuestInactive 9004
+    QuestInactive 9005
+    QuestInactive 9006
+    QuestInactive 9007
+    QuestInactive 9008
+    QuestInactive 9009
+    QuestInactive 9010
+    QuestInactive 9011
+    QuestInactive 9012
+    ConfigKeyNot virarClasse2 true
+    NpcNotNear /Chefe dos Cavaleiros/
+    call {
+        do move prt_in 88 101
+    }
+}
+
+automacro virarCavaleiro_Começo_FalarcomNPC {
+    JobLevel = 50
+    JobID $paramsClasses{idC1}
+    exclusive 1
+    QuestInactive 9000
+    QuestInactive 9001
+    QuestInactive 9002
+    QuestInactive 9003
+    QuestInactive 9004
+    QuestInactive 9005
+    QuestInactive 9006
+    QuestInactive 9007
+    QuestInactive 9008
+    QuestInactive 9009
+    QuestInactive 9010
+    QuestInactive 9011
+    QuestInactive 9012
+    NpcNotNear /Chefe dos Cavaleiros/
+    call {
+        do talk $.NpcNearLastBinId
+        do talk resp 0 #checar a respostas!!!
+        do talk resp 0 #checar as respostas!!!
+        [
+        log ==============================
+        log Falei com o Chefe dos Cavaleiros, agora 
+        log vou falar com o Cavaleiro Dedicado!
+        log ==============================
+        ]
+    }
+}
